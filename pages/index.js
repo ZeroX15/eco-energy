887664a2cb45b6b28d8482ba0edccba22e0a434b
ZeroX15 <zecarvalho@nexusnx.com>
2025-01-06 17:49:44 -0400

    Primeiro commit: estrutura inicial do site.

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+/* TODO: 
+ - Criar o "site" na estrutura padrão HTML-CSS-JS
+ - Converter ou migrar para TypeScript
+ - Converter para React
+ - Converter para Next
+
+   Tudo isso com o intuito de aprender e mostrar meu domínio em diferentes linguagens de programação.
+*/
